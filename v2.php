4146d2dc4480a0a41528ee0ea75a84de030a922c
SEO777 <kangunben5@gmail.com>
2026-03-07 17:08:20 +0800

    Update v2.php

--- v2.php
+++ v2.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $PASSWORD = "admin123";  // default Password
 session_start();
 define('SCRIPT_DIR', __DIR__);
